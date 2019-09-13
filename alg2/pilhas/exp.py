134bd2a6601362d0dbf95f213ef557ec653598e8
drawiin <vini65599@gmail.com>
2019-09-13 00:24:54 -0400

    okay

--- alg2/pilhas/exp.py
+++ alg2/pilhas/exp.py
@@ -4,14 +4,15 @@ pilha = []
 exp = exp.replace(" ", "")
 
 for i in exp:
-    if i.isalnum():
-        if len(pilha) > 0 and pilha[-1].isalnum():
+    if i.isnumeric():
+        if len(pilha) > 0 and pilha[-1].isnumeric():
             pilha[-1] += i
         else:
             pilha.append(i)
     elif i == '+' or i == '-' or i == '(':
         pilha.append(i)
     elif i == ')':
+        break
         while pilha[-1] != '(':
             pass
 
