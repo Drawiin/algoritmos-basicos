95f4c812eaee80760e873aab3625b0a85d3c30c2
drawiin <vini65599@gmail.com>
2019-09-16 19:45:26 -0400

    fila Prioridade

---
+++ alg2/filas/filaPrioridade.py
@@ -0,0 +1,51 @@
+class Pessoa:
+    def __init__(self, nome, prioridade=0):
+        self.nome = nome
+        self.prioridade = prioridade
+
+
+class Fila:
+    def __init__(self):
+        self.filaComum = []
+        self.filaPrioritaria = []
+
+    def insere(self, nome, prioridade=0):
+        if prioridade == 0:
+            self.filaComum.append(Pessoa(nome, prioridade))
+        elif prioridade == 1:
+            self.filaPrioritaria.append(Pessoa(nome, prioridade))
+        else:
+            print('Prioridade Inválida')
+
+    def chamaPessoa(self):
+        if len(self.filaPrioritaria) > 0:
+            print('Atendendo:', self.filaPrioritaria.pop(0).nome)
+            self.imprimeFila()
+
+        elif len(self.filaComum) > 0:
+            print('Atendendo', self.filaComum.pop(0).nome)
+            self.imprimeFila()
+
+        else:
+            print('Fila vazia :/')
+
+    def imprimeFila(self):
+        for pessoa in self.filaPrioritaria:
+            print('nome:', pessoa.nome)
+
+        for pessoa in self.filaComum:
+            print('nome:', pessoa.nome)
+
+
+if __name__ == "__main__":
+    fila = Fila()
+    for i in range(10):
+        fila.insere('roger'+str(i))
+
+    for i in range(3):
+        fila.insere('Altair'+str(i), 1)
+
+    for i in range(10):
+        print('-'*20)
+        fila.chamaPessoa()
+    
