7be85f3a29d408f3db166ad023ab84dc4eac6115
drawiin <vini65599@gmail.com>
2020-04-07 13:06:02 -0400

    creando script de  setup do ambiente

---
+++ BashScript/setup.sh
@@ -0,0 +1,4 @@
+# Um script que uso para intalar todos os programas que utilizo no linux(Ubunto)
+# nvm -> nodejs
+# vscode -> instalar extensões code --install-extension 
+# git -> (config user and emal -> create ssh key)
