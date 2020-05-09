644814021a03d1d6458ed82cbd54260bb3774560
Vinicius Santos <vini65599@gmail.com>
2020-05-08 23:00:40 -0400

    implementing user terminal interface

--- Python/OnibusFlexivel/routeCalculator.py
+++ Python/OnibusFlexivel/routeCalculator.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from math import factorial
 from itertools import permutations
 
 
@@ -26,7 +27,33 @@ def calculateBestRoute(route):
     return (bestRoute, bestRouteLength)
 
 
+def readPoint(index):
+    return tuple([int(i) for i in input('ponto {} entre com x e y separados por espaço: '.format(index + 1)).split(' ')])
+
+
 if __name__ == "__main__":
-    path = [(0, 0), (2, 2), (5, 4), (6, 3), (8, 3), (10, 10)]
-    passangers = [(5, 2), (6, 2), (4, 5), (7, 6), (8, 6)]
-    print(calculateBestRoute(path))
+    pathLength = int(input('Quantos pontos o caminho do ônibus tem ? '))
+    passangersLength = int(input('Quantos pontos de passageiros tem ?'))
+    print('Entre com os pontos da rota do ônibus')
+    path = [readPoint(i) for i in range(pathLength)]
+    print('Entre com os pontos dos passageiros')
+    passangers = [readPoint(i) for i in range(passangersLength)]
+
+    print("pontos  da rota do ônibus")
+    print(path)
+
+    print("pontos  da rota dos passageiros")
+    print(passangers)
+
+    input('Pressione enter para continuar')
+
+    route = path + passangers
+
+    if(len(route) > 8):
+        input('A rota que você está prestes a calcular contem vários pontos\nisso pode levar vários minutos ........ ou anos\npressione enter para continuar')
+    print('calculando......', factorial(len(route)), 'rotas possiveis')
+    result = calculateBestRoute(route)
+    print('A melhor rota encontrada foi')
+    print(result[0])
+    print('Com custo de ')
+    print(result[1])
