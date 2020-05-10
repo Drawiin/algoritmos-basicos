0075bf33ad7703bceb028bdd4ac20654c9126b08
Vinicius Santos <vini65599@gmail.com>
2020-05-10 16:31:09 -0400

    ajusting setup script

--- BashScript/setup.sh
+++ BashScript/setup.sh
@@ -4,26 +4,32 @@
 
 # atuazliar os ppas
 echo 'iniciando.........'
+echo '============================atulizando ppas===================================='
 sudo apt update -y
 
 # build-essesntial
+echo '============================instalando build essentials===================================='
 apt install build-essential -y
 
 # git
+echo '===================intalando git.======================'
 apt install git -y
 
 # curl
+echo '============================Istalando curl===================================='
 apt install curl -y
 
 #wget
-
+echo '============================Instalando Wget===================================='
 apt install wget -y
 
 # chrome
+echo '============================Instalando chrome===================================='
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb -y
 
 # nvm
+echo '============================Instalando nvm===================================='
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -33,25 +39,32 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 # golang
 # ubunto kvm
 # vscode
-snap install code --classic -y
+echo '============================ instalando vscode===================================='
+snap install code --classic
 
 # qbittorent
+echo '============================instalando qbittorrent===================================='
 add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 apt-get update && sudo apt-get install qbittorrent -y
 
 # spotfy
-snap install spotify -y
+echo '============================  spotfy ===================================='
+snap install spotify
 
 # vlc
-snap install vlc -y
+echo '============================ vlc ===================================='
+snap install vlc
 
 # insomnia
-snap install insomnia -y
+echo '============================ insomnia ===================================='
+snap install insomnia
 
 # Intelij idead Comminity
-sudo snap install intellij-idea-community --classic -y
+echo '============================ Intellijidea ===================================='
+sudo snap install intellij-idea-community --classic
 
 # postgreSQL
+echo '============================PostegreSQL===================================='
 sudo apt install postgresql postgresql-contrib -y
 
 # oh my z shell
